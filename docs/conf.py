427cee4a9b443ed742fe72c6a731d76dced218a2
raymondwangli <>
2020-10-15 23:26:49 -0400

    Update conf.py to find doc file

--- docs/conf.py
+++ docs/conf.py
@@ -25,6 +25,7 @@ author = 'Raymond Li'
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
 
+master_doc='index'
 
 # -- General configuration ---------------------------------------------------
 
