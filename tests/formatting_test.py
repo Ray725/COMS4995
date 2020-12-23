50136fe549ce7a5cd12b7e1f9620f754be54f399
raymondwangli <>
2020-12-22 20:31:43 -0500

    Add coverage for formatting helper function

--- tests/formatting_test.py
+++ tests/formatting_test.py
@@ -1,4 +1,5 @@
 from formatting.format_helper import format_message
+from formatting.format_helper import format_message_helper
 
 class TestFormatting:
     def test_format_message_error_handling(self):
@@ -7,3 +8,10 @@ class TestFormatting:
     def test_format_message(self):
         expected_output = '<b>a</b>\n<i>b</i>\n1: c\n'
         assert format_message('a', 'b', ['c']) == expected_output
+
+    def test_format_message_helper_error_handling(self):
+        assert format_message_helper([]) == None
+
+    def test_format_message_helper(self):
+        expected_output = '<b>a</b>\n<i>b</i>\n1: c\n'
+        assert format_message_helper(['a', 'b', 'c']) == expected_output
